430886cb7f504781d359b461e2a26a698c582397
r1v3r <churiver@outlook.com>
2022-08-04 00:36:28 +0800

    feat: optimize mem show

--- autostart.sh
+++ autostart.sh
@@ -5,5 +5,8 @@
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom -b
 /bin/bash ~/scripts/setxmodmap-colemak.sh &
+/bin/bash ~/scripts/setup.sh &
 nitrogen --restore &
 fcitx5 -d
+
+
